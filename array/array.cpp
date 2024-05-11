1360eb7f74ea649b6eb6db12e8d4d5c42aece86d
iskandervop@gmail.com <iskandervop@gmail.com>
2024-05-11 15:12:06 +0300

    бла бла бла

---
+++ array/array.cpp
@@ -0,0 +1,15 @@
+﻿// array.cpp: определяет точку входа для приложения.
+//
+
+#include "array.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	methodearray();
+
+	return 0;
+}
---
+++ array/array.h
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class methodearray
+{
+private:
+
+public:
+     methodearray(/* args */);
+     ~methodearray();
+};
+
+methodearray::methodearray(/* args */)
+{
+    vector<int> array;
+    // numofarraynaum это числа для того что бы знать длинну массива. numfornumofarray это числа самого массива
+    int numofarraynaum, numfornumofarray ;
+    int arraynum = 1;
+    //вводим числа длинны массива
+    cout << "enter the length of the array" << endl;
+    cin >> numofarraynaum;
+    //вводим числа маасива и сразу записывем их в массив
+    cout << "enter numbers array" << endl;
+    for (int y{ 0 }; y < numofarraynaum; y++)
+    {
+        cin >> numfornumofarray;
+        array.push_back(numfornumofarray);
+    }
+
+    //достаем числа из массива и унажаем значения миссива 
+    for (int i{ 0 }; i < array.size(); i++)
+    {
+        arraynum *= array[i];
+    }
+
+
+    cout << "multiply array evaluations" << arraynum << endl;
+    
+}
+
+methodearray::~methodearray()
+{
+
+
+}
